21230b6d786955d88ff9e34a06300afcc370f482
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2023-11-15 16:00:24 +0100

    yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
+  get 'reviews/rating'
+  get 'reviews/content'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:create]
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "restaurants", to: "restaurants#index"
+
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create", as: :created_restaurant
+
+  get "restaurants/:id", to: "restaurants#show"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+restaurants_attributes = [
+  { name: "Chez Margot", address: "123 Rue de Paris, Paris", phone_number: "0123456789", category: "french" },
+  { name: "La Trattoria", address: "456 Via Roma, Rome", phone_number: "0123456790", category: "italian" },
+  { name: "Sushi Place", address: "789 Nishi Shinjuku, Tokyo", phone_number: "0123456791", category: "japanese" },
+  { name: "Dragon Wok", address: "1010 East Nanjing Road, Shanghai", phone_number: "0123456792", category: "chinese" },
+  { name: "Brussels Sprouts", address: "1313 Rue des Bouchers, Brussels", phone_number: "0123456793", category: "belgian" }
+]
+
+restaurants_attributes.each do |attributes|
+  Restaurant.find_or_create_by!(attributes)
+end
+
+puts 'Finished seeding the database with restaurants!'
